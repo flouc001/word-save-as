8ff7b9a57bdaa292693c68d0af6c30e306d62fc5
flouc001 <flouc001@gmail.com>
2017-07-11 12:08:00 +0100

    rename to wordsaveas

--- wordsavesas/Properties/AssemblyInfo.cs
+++ wordsavesas/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("wordsavesas")]
+[assembly: AssemblyTitle("wordsaveas")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("wordsavesas")]
+[assembly: AssemblyProduct("wordsaveas")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
